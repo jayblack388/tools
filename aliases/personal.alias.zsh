f7613c41676f71a2b03e7eb8e172edf86963ad48
John Blackwell <jblackwell072588@gmail.com>
2021-08-24 13:49:59 -0700

    removing some old/unused aliases

--- aliases/personal.alias.zsh
+++ aliases/personal.alias.zsh
@@ -8,15 +8,6 @@ alias jdb-workspace="code ~/Documents/code/workspaces/jdb.code-workspace"
 
 alias prettierConfig="cp ~/Documents/code/workspaces/.prettierrc ./"
 
-alias clearprofile='ssh-add -D'
-alias jayblack='git config user.email jblackwell072588@gmail.com && git config user.name "John Blackwell" && ssh-add ~/.ssh/id_rsa_personal'
-alias johnbqc='git config user.email jblackwell1@q-centrix.com && git config user.name "John Blackwell" && ssh-add ~/.ssh/id_rsa'
-
-# Should rework this in some way
-alias tutoring='code ~/Documents/code/side/personal-repos/tutoring/fullstack-ground && open https://docs.google.com/spreadsheets/d/1XmevsIF1sMhJjg3Gdtr-7uXO3VSjjRlrKW2ygBMxdi8/ && open https://workforcenow.adp.com/workforcenow/login.html && tut'
-
-alias tut='node ~/Documents/code/side/personal-repos/tutoring/tutoring_script'
-
 alias flipped='echo "(╯°□°)╯︵ ┻━┻"'
 
 alias shocked="
@@ -41,8 +32,6 @@ echo '(•_•)'
 echo '( •_•)>⌐■-■'
 echo '(⌐■_■)'
 "
-
-# alias mkComp='node ~/Documents/code/scripts/jdb-component'
 
 newtabi(){  
   osascript \
